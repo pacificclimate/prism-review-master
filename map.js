40a245bafd0ddadddeee9391b14d194fdefdc00a
Basil Veerman <bveerman@uvic.ca>
2015-02-11 15:53:46 -0800

    Separate map config, add layer select to report, convert form to JSON on submit

---
+++ map.js
@@ -0,0 +1,75 @@
+var crs = new L.Proj.CRS.TMS(
+  'EPSG:3005',
+  '+proj=aea +lat_1=50 +lat_2=58.5 +lat_0=45 +lon_0=-126 +x_0=1000000 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs',
+  [-1000000, -1000000, 3000000, 3000000],
+  { 
+    resolutions: [7812.5, 3906.25, 1953.125, 976.5625, 488.28125, 244.140625, 122.0703125, 61.03515625, 30.517578125, 15.2587890625, 7.62939453125, 3.814697265625]
+  }
+);
+
+var map = new L.Map('map', 
+  {
+    crs: crs,
+    center: new L.LatLng(55, -125), 
+    zoom: 2,
+    maxBounds: L.latLngBounds([[45, -148], [62, -108]]),
+    worldCopyJump: false
+  }
+  );
+
+var osm = L.tileLayer('http://{s}.tiles.pacificclimate.org/tilecache/tilecache.py/1.0.0/bc_osm/{z}/{x}/{y}.png', 
+  {
+    subdomains: 'abc',
+    maxZoom: 12,
+    noWrap: true,
+    attribution: '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    continuousWorld: false
+  }
+  ).addTo(map);
+
+var pr = L.tileLayer.wms('http://atlas.pcic.uvic.ca/ncWMS/wms', {
+  layers: 'pr_monClim_PRISM_historical_run1_197101-200012/pr',
+  transparent: true,
+  format: 'image/png',
+  time: '1985-06-30',
+  styles: 'boxfill/occam_inv',
+  logscale: true,
+  numcolorbands: 254,
+}).addTo(map);
+var tmax = L.tileLayer.wms('http://atlas.pcic.uvic.ca/ncWMS/wms', {
+  layers: 'tmax_monClim_PRISM_historical_run1_197101-200012/tmax',
+  transparent: true,
+  format: 'image/png',
+  time: '1985-06-30',
+  styles: 'boxfill/ferret',
+  logscale: false,
+  numcolorbands: 254,
+});
+var tmin = L.tileLayer.wms('http://atlas.pcic.uvic.ca/ncWMS/wms', {
+  layers: 'tmin_monClim_PRISM_historical_run1_197101-200012/tmin',
+  transparent: true,
+  format: 'image/png',
+  time: '1985-06-30',
+  styles: 'boxfill/ferret',
+  logscale: false,
+  numcolorbands: 254,
+});
+
+// var prismLayers = L.layerGroup([pr, tmax, tmin]).addTo(map);
+var groupedOverlays = {
+  "PRISM Climatologies": {
+    "Max Temp": tmax,
+    "Min Temp": tmin,
+    "Precip": pr,
+  }
+};
+var options = { exclusiveGroups: ["PRISM Climatologies"] };
+L.control.groupedLayers(null, groupedOverlays, options).addTo(map);
+
+var drawnItems = L.featureGroup().addTo(map);
+
+L.control.mousePosition().addTo(map);
+
+map.addControl(new L.Control.Draw({
+ edit: { featureGroup: drawnItems }
+}));
